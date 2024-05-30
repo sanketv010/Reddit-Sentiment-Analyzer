606d6bf20ee90f2b0236c9b1b614de8340d9bdf4
sanketv010 <sanketverma425@gmail.com>
2024-05-30 22:25:20 +0530

    flask app changes

--- application.py
+++ application.py
@@ -45,5 +45,5 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction=None)  
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)            
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0", debug=True)            
